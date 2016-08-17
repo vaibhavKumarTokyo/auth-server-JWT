72c0d00af327b5caf6dd12a3d38ef20a19ac21dc
Vaibhav.Kumar <=>
2016-08-17 15:59:24 +0900

    modularized

---
+++ app/routes/api.js
@@ -0,0 +1,132 @@
+//
+//PACKAGES
+//
+var express = require('express');
+var User   = require('../models/user'); // get our mongoose model
+var mongoose    = require('mongoose');
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('config'); // get our config file
+
+//
+//CONFIGURATION
+//
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('Connected to DB!');
+});
+mongoose.connect(config.database); // connect to database
+//
+// API ROUTES -------------------
+//
+var apiRoutes = express.Router();
+
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+apiRoutes.post('/authenticate', function(req, res) {
+  console.log(`req.body.name ${req.body.name}`);
+  console.log(`User: ${JSON.stringify(User)}`);
+  // find the user
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+
+    if (err) throw err;
+
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+
+      // check if password matches
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+
+        // if user is found and password is right
+        // create a token
+        var userIdentity = {"name": user.name};
+        var token = jwt.sign(userIdentity, config.secret, {
+          //expiresInMinutes: 1440 // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }
+
+    }
+
+  });
+});
+
+// route middleware to verify a token
+apiRoutes.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, config.secret, function(err, decoded) {
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({
+        success: false,
+        message: 'No token provided.'
+    });
+
+  }
+});
+
+
+// route to show a random message (GET http://localhost:8080/api/)
+apiRoutes.get('/', function(req, res) {
+  res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+apiRoutes.get('/users', function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+});
+
+//
+// SETUP FOR DEV
+//
+apiRoutes.get('/setup', function(req, res) {
+
+  // create a sample user
+  var nick = new User({
+    name: 'Nick Cerminara',
+    password: 'password',
+    admin: true
+  });
+
+  // save the sample user
+  nick.save(function(err) {
+    if (err) throw err;
+
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
+});
+
+
+module.exports = apiRoutes;
